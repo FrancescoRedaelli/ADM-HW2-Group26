877aad8c6dee9ca345e5b140d8d57f426d4f097c
FrancescoRedaelli <redaelli.2039054@studenti.uniroma1.it>
2022-10-23 17:12:03 +0200

    Completed RQ3, Added RQ4

--- functions.py
+++ functions.py
@@ -1,18 +1,177 @@
+import pandas as pd   # Data manipulation and analysis
+import numpy as np    # Scientific Computing
+
 import matplotlib.pyplot as plt   # Visualization
+from tqdm import tqdm   # Progress bar
+
+def initialize_posts_reader():
+    return pd.read_csv(r'D:\Data\instagram_posts.csv', delimiter='\t', chunksize=100000, converters={"profile_id": str, "location_id": str})
 
 ###[RQ3]
 
+def retrieve_posts_time(posts_reader):
+
+    '''
+    Retrieve the posts publishing time
+    '''
+
+    posts_time = []
+
+    for chunk in posts_reader:
+        hour_series = pd.to_datetime(chunk.cts).dt.hour
+        posts_time.extend(hour_series.tolist())
+
+    return posts_time
+
 def plot_posts_time(posts_time):
+
     '''
     Plot the daily temporal distribution of posts published
     '''
+
+    # Plot
     f = plt.figure()
-    plt.xticks(range(0,25),["{}:00".format(x).zfill(5) for x in range(0,25)], rotation = 45)
-    plt.ylabel("# posts published", fontsize=14, labelpad=20)
+    plt.xticks(range(25), ["{}:00".format(x).zfill(5) for x in range(25)], rotation = 45)
+    plt.ylabel("# posts published (Millions)", fontsize=14, labelpad=20)
     plt.xlabel("Time of the day", fontsize=14, labelpad=20)
-    plt.title("Daily temporal distribution of posts published", fontsize=18)
+    plt.title("Daily temporal distribution of posts published", fontsize=18, pad=15)
     plt.hist(posts_time, bins=range(25), color='#00ff00', ec="k")
+    plt.yticks(plt.gca().get_yticks(), [round(x/1e6,1) for x in plt.gca().get_yticks()])
     f.set_figwidth(14)
     f.set_figheight(8)
+
+    return
+
+def plot_posts_time_custom(posts_time, time_intervals, ftype=""):
+
+    '''
+    Plot the distribution of posts published per time interval
+    '''
+
+    # Input: time intervals list; each time interval is of the form [Start, End)
+
+    intervals_out = {}
+
+    for t in time_intervals:
+        # Compute #posts per time interval
+        intervals_out[t] = len([x for x in posts_time if t[0] <= x < t[1]])
+
+    # Sort dict by key (time interval) for visualization purposes
+    intervals_out = dict(sorted(intervals_out.items()))
+    # Build time interval strings
+    intervals_list = ["{}:00".format(x[0]).zfill(5) + " - " + "{}:00".format(x[1]).zfill(5) for x in intervals_out.keys()]
+
+    # Plot
+    f = plt.figure()
+    plt.xticks(range(len(time_intervals)), intervals_list, rotation = 45)
+    plt.ylabel("# posts published{}".format(" (Millions)" if ftype!="top10" else ""), fontsize=14, labelpad=20)
+    plt.xlabel("Time interval", fontsize=14, labelpad=20)
+    plt.title("Distribution of posts published per time interval{}".format(" of the top10 profiles by #posts" if ftype!="top10" else ""), fontsize=18, pad=15)
+    plt.bar(intervals_list, intervals_out.values(), color='#ff6600', ec="k")
+    if ftype!="top10": plt.yticks(plt.gca().get_yticks(), [round(x/1e6,1) for x in plt.gca().get_yticks()])
+    f.set_figwidth(14)
+    f.set_figheight(8)
+
+    return
+
+###[RQ4]
+
+def build_profile_posts_map(posts_reader):
+
+    '''
+    Build the profile_id-posts dictionary
+    '''
+
+    mymap = dict()
+
+    for chunk in posts_reader:
+        ids, post_id, cts, nlikes, ncomm = chunk.profile_id.tolist(), chunk.post_id.tolist(), chunk.cts.tolist(), chunk.numbr_likes.tolist(), chunk.number_comments.tolist()
+        for i in range(len(ids)):
+            if str(ids[i]) in mymap: mymap[str(ids[i])].append([post_id[i], cts[i], nlikes[i], ncomm[i], str(ids[i])])
+            else: mymap[str(ids[i])] = [[post_id[i], cts[i], nlikes[i], ncomm[i], str(ids[i])]]
+
+    return mymap
+
+def posts_by_target_profile_ids(profile_posts_map, profile_id):
+
+    '''
+    Retrieve the posts belonging to the target profile_id
+    '''
+
+    if str(profile_id) not in profile_posts_map: return []
+    return profile_posts_map[str(profile_id)]
+
+def posts_by_top_profile_ids(profile_posts_map, profiles_df, n):
+
+    '''
+    Retrieve the posts belonging to the n top posted profiles for which data is available
+    '''
+
+    # Get unique profiles id
+    profiles_available = set(profiles_df.profile_id)
+
+    posts = []
+
+    for id in profiles_available:
+        # Retrieve posts by current profile
+        curr_posts = posts_by_target_profile_ids(profile_posts_map, id)
+        # If less than n profiles analyzed, store posts
+        if len(posts)<n: posts.append(curr_posts)
+        else:
+            # Check if #posts is strictly more than the #posts of the bottom posted profile stored
+            if len(posts[n-1])<len(curr_posts):
+                # Remove the posts by bottom posted profile
+                posts.pop()
+                # Store the posts by current profile
+                posts.append(curr_posts)
+                # Sort stored posts by length in descending order
+                posts.sort(key=len, reverse = True)
+
+    return posts
+
+def plot_avg_stats_top10(posts):
+
+    '''
+    Plot the avg number of likes and comments of the top10 profiles by #posts
+    '''
+
+    # Input: lists of posts of the top10 profiles by #posts
+
+    avg_likes = []
+    avg_comments = []
+
+    profiles_ids = []
+
+    # Compute avg #likes and #comments for each profile
+    for profile in posts:
+        profiles_ids.append((profile[0]).pop())
+        values = list(map(lambda x: sum(x)/len(x), zip(*[[x[2],x[3]] for x in profile])))
+        avg_likes.append(values[0])
+        avg_comments.append(values[1])
+
+    # Plot
+    f = plt.figure()
+    xticks = range(len(avg_likes))
+    plt.xticks(xticks, profiles_ids, rotation = 45)
+    plt.ylabel("Average value", fontsize=14, labelpad=20)
+    plt.xlabel("Profile ID", fontsize=14, labelpad=20)
+    plt.title("Average number of likes and comments of the top10 profiles by #posts", fontsize=18, pad=15)
+    plt.bar([x-0.2 for x in xticks], avg_likes, width=0.4, color='#00ccff', ec="k")
+    plt.bar([x+0.2 for x in xticks], avg_comments, width=0.4, color='#ff0000', ec="k")
+    plt.legend(["Likes", "Comments"], fontsize=14, loc="upper left")
+    f.set_figwidth(14)
+    f.set_figheight(8)
+
+    return
+
+def plot_posts_top10(posts, time_intervals):
+
+    posts_time = []
+
+    for profile in posts:
+        hours = [pd.to_datetime(x[1]).hour for x in profile]
+        posts_time.extend(hours)
+
+    plot_posts_time_custom(posts_time, time_intervals, ftype="top10")
 
     return
