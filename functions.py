717a4ad14e4c6b52b8a4ce4a2aa02c4c34d0e28e
FrancescoRedaelli <redaelli.2039054@studenti.uniroma1.it>
2022-10-27 20:30:04 +0200

    BP(a) + Adjustments

--- functions.py
+++ functions.py
@@ -2,7 +2,12 @@ import pandas as pd   # Data manipulation and analysis
 import numpy as np    # Scientific Computing
 
 import matplotlib.pyplot as plt   # Visualization
+import matplotlib.lines as mlines   # Visualization
+
 from tqdm import tqdm   # Progress bar
+
+from collections import defaultdict   # Dictionary with default value
+from datetime import datetime   # Date and Time handling
 
 def initialize_posts_reader():
     return pd.read_csv(r'D:\Data\instagram_posts.csv', delimiter='\t', chunksize=100000, converters={"profile_id": str, "location_id": str})
@@ -84,7 +89,7 @@ def build_profile_posts_map(posts_reader):
 
     mymap = dict()
 
-    for chunk in posts_reader:
+    for chunk in tqdm(posts_reader):
         ids, post_id, cts, nlikes, ncomm = chunk.profile_id.tolist(), chunk.post_id.tolist(), chunk.cts.tolist(), chunk.numbr_likes.tolist(), chunk.number_comments.tolist()
         for i in range(len(ids)):
             if str(ids[i]) in mymap: mymap[str(ids[i])].append([post_id[i], cts[i], nlikes[i], ncomm[i], str(ids[i])])
@@ -166,6 +171,10 @@ def plot_avg_stats_top10(posts):
 
 def plot_posts_top10(posts, time_intervals):
 
+    '''
+    Plot the number of posts sent by top 10 profiles by #posts in the given interval in [RQ3]
+    '''
+
     posts_time = []
 
     for profile in posts:
@@ -173,5 +182,71 @@ def plot_posts_top10(posts, time_intervals):
         posts_time.extend(hours)
 
     plot_posts_time_custom(posts_time, time_intervals, ftype="top10")
+
+    return
+
+###BP(a)
+
+def plot_two_categories(profiles_df, posts_reader):
+
+    '''
+    Plot the mean of time intervals between posts for top 10% profiles by #followers VS bottom 90%
+    '''
+
+    # Sort profiles by number of followers (Descending)
+    profiles_df = profiles_df.sort_values(by="followers", ascending=False)
+
+    # Compute the 10% number of profiles
+    top10_value = int(len(profiles_df)/10)
+
+    # Divide profiles by followers in two sets (for fast existence checking)
+    top_profiles = set(profiles_df.head(top10_value)["profile_id"])
+    bottom_profiles = set(profiles_df.tail(len(profiles_df)-top10_value)["profile_id"])
+
+    # Retrieve posts times for the two categories
+    top_dict = defaultdict(list)
+    bottom_dict = defaultdict(list)
+
+    times_list = []
+
+    for chunk in posts_reader:
+        ids, cts = chunk.profile_id.tolist(), chunk.cts.tolist()
+        times_list.extend([(cts[i], ids[i]) for i in range(len(ids))])
+
+    # Remove NaNs
+    times_list = [x for x in times_list if str(x[0])!="nan"]
+
+    # Sort by posts times
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    times_list.sort(key=lambda x: datetime.strptime(str(x[0]), date_format))
+
+    # Create dict of (sorted) posts times of each profile for each category
+    for pair in times_list:
+        if pair[1] in top_profiles: top_dict[pair[1]].append(pair[0])
+        if pair[1] in bottom_profiles: bottom_dict[pair[1]].append(pair[0])
+
+    # Compute time intervals
+    top_intervals = []
+    bottom_intervals = []
+
+    for profile in top_dict.keys():
+        top_intervals.extend(((pd.to_datetime(pd.Series(top_dict[profile]))).diff().dt.total_seconds().div(3600).tolist())[1:])
+
+    for profile in bottom_dict.keys():
+        bottom_intervals.extend(((pd.to_datetime(pd.Series(bottom_dict[profile]))).diff().dt.total_seconds().div(3600).tolist())[1:])
+
+    # Plot
+    f, ax = plt.subplots()
+    boxplot = ax.boxplot([top_intervals, bottom_intervals], showmeans=True, notch=False, vert=True, patch_artist=True, labels=["Top 10%", "Bottom 90%"])
+    for patch, color in zip(boxplot['boxes'], ['lightblue', 'pink']): patch.set_facecolor(color)
+    ax.yaxis.grid(True)
+    ax.set_ylabel("Time Interval Length (Hours)", fontsize=14, labelpad=20)
+    ax.set_xlabel("Category", fontsize=14, labelpad=20)
+    ax.set_ylim(0, 700)
+    green_triangle = mlines.Line2D([], [], color='green', marker='^', linestyle='None', markersize=14, label='Mean')
+    ax.legend(handles=[green_triangle], fontsize=14)
+    plt.title("Distribution of time intervals between posts (top 10% VS bottom 90% profiles)", fontsize=18, pad=15)
+    f.set_figwidth(14)
+    f.set_figheight(8)
 
     return
